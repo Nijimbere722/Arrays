107c75c6dbeb517131b51d0cacbd74bd526aabd7
unknown <nijimberemonia04@gmail.com>
2026-02-17 13:03:16 +0000

    First Example

---
+++ script.js
@@ -0,0 +1,15 @@
+const productCategories = ["Books", "Electronics","clothing" , "Home & Kitchen", "Toys & Games"];
+// a
+console.log(ProductCategories[0]);
+
+// b
+console.log(productCategories[productCategories.length -1]);
+// c
+console.log(productCategories.length);
+// d
+productCategories[1]= "Pen"
+// e
+console.log(productCategories);
+
+
+
